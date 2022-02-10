1c849ebcce0dde36e78af409e2923a318ebf5aa9
sreelakshmi2001 <90597983+sreelakshmi2001@users.noreply.github.com>
2022-02-10 21:26:11 +0530

    Create add.cpp

---
+++ add.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    int n1, n2, sum,diff,mul;
+
+cout<<"enter the numbers:"<<endl;
+cin>>n1;
+cin>>n2;
+sum=n1 + n2;
+diff= n1- n2;
+mul= n1*n2;
+div= n1/n2;
+cout<< "the sum is"<<sum<<endl;
+cout<< "the difference is"<<diff<<endl;
+cout<< "the multiple is"<<mul<<endl;
+cout<<" the quotient is"<<div<<endl;
+
+
+return 0;
+
+
+    }
